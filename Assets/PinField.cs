6ba341ee54dddcba995a1d6fb9b3655a8be2d502
Edvin <edvin.klaebo@gmail.com>
2016-11-18 22:09:23 +0100

    Trash Spewer and Bowling Pins

---
+++ Assets/PinField.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinField : MonoBehaviour
+{
+    public GameObject pinObject;
+
+    public float rotationAngle;
+
+    protected void Start()
+    {
+        Setup();
+    }
+
+    public void Setup()
+    {
+        for(int i = 1; i <= 4; i++)
+        {
+            for(int j = i; j <= 4; j++)
+            {
+                Instantiate(pinObject, new Vector3(
+                                1.12f * i + transform.position.x, 0 + transform.position.y, 
+                                1.12f * j + transform.position.z), Quaternion.identity, gameObject.transform );
+            }
+        }
+
+        transform.Rotate(new Vector3(0, rotationAngle, 0));
+    }
+
+}
---
+++ Assets/Scripts/TrashSpewer.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TrashSpewer : MonoBehaviour
+{
+    public GameObject[] TrashItems;
+    public int MaxSpewedItems = 100;
+
+    public bool randomSpew = false;
+    public Vector3 SpewForce = new Vector3(0, 1000, 0);
+
+    private int currentSprewedItems = 0;
+    private List<GameObject> itemsOnGround = new List<GameObject>();
+    private bool shouldSpawnTrash = true;
+
+    protected void Update()
+    {
+        if (!shouldSpawnTrash) return;
+        if (currentSprewedItems > MaxSpewedItems)
+        {
+            Invoke("RemoveTrashFromWorld", 5.0f);
+            shouldSpawnTrash = false;
+        }
+
+        var randomTrashItem = Random.Range(0, TrashItems.Length);
+        var trashitem = Instantiate(TrashItems[randomTrashItem]);
+
+        itemsOnGround.Add(trashitem);
+
+        var trashitemRigidBody = trashitem.GetComponent<Rigidbody>();
+        if (randomSpew)
+            trashitemRigidBody.AddForce(new Vector3(
+                SpewForce.x * Random.Range(-10, 10), 
+                SpewForce.y * Random.Range(0, 10), 
+                SpewForce.z * Random.Range(-10, 10)));  
+        else
+            trashitemRigidBody.AddForce(SpewForce);
+
+        currentSprewedItems++;
+    }
+
+    public void RemoveTrashFromWorld()
+    {
+        foreach(var item in itemsOnGround)
+        {
+            Destroy(item);
+        }
+    }
+
+    public void Reset()
+    {
+        shouldSpawnTrash = true;
+        currentSprewedItems = 0;
+    }
+
+}
