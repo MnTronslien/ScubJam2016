d7eda6d8e5a9ddf1518dae4b54036c476ef01108
Ryan <ryan.wheeler86@gmail.com>
2016-11-18 21:26:14 +0100

    Initial Rubber-Band Mouse-Drag Launch Controls

---
+++ Assets/Scripts/LaunchBall.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LaunchBall : MonoBehaviour {
+
+    public float force = 0f;
+    
+    public Transform origin;
+    public Rigidbody target;
+
+    public string launchEvent = "launchBall";
+
+    public static LaunchBall instance;
+
+	void Awake ()
+    {
+        if( instance == null )
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+            return;
+        }
+	}
+
+	void Update ()
+    {
+	
+	}
+
+    public void Launch( Vector3 velocity )
+    {
+        this.target.AddForce(this.force * velocity);
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour 
+{
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	    if( Input.GetKeyDown( KeyCode.R ) )
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+}
+
