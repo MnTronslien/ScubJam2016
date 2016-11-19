51b1d272fcc4415ce43a689c5c784c2a101155d6
Edvin <edvin.klaebo@gmail.com>
2016-11-19 16:01:13 +0100

    Particles speed and scale powerups

---
+++ Assets/Scripts/SpeedPowerUp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedPowerUp : MonoBehaviour
+{
+    public float SpeedBoostFactor = 4;
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.GetComponent<Rigidbody>() != null)
+        {
+            var rigidBody = col.GetComponent<Rigidbody>();
+            var vel = rigidBody.velocity;
+            vel = vel * SpeedBoostFactor;
+
+            rigidBody.velocity = vel;
+        }
+    }
+
+}
