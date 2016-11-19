14973238f04f9b0acca9f9db164c0fd5a7b6020d
Gunnar A. Holst <holgun14@student.westerdals.no>
2016-11-19 17:01:10 +0100

    Added camcontroller script and overviewcamera-script.

---
+++ Assets/Scripts/BigDisplayController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum BigDisplayCam
+{
+    OverviewCam,
+    FollowCam
+}
+
+public class BigDisplayController : MonoBehaviour {
+
+	public BigDisplayCam CurrentCam;
+    public GameObject[] Cameras;
+
+    void Start()
+    {
+        SwapCam(BigDisplayCam.OverviewCam);
+    }
+
+    public void SwapCam(BigDisplayCam cameraType)
+    {
+        ActivateCam(false);
+        CurrentCam = cameraType;
+        ActivateCam(true);
+    }
+
+    private void ActivateCam(bool activate)
+    {
+        Cameras[(int) CurrentCam].GetComponent<Camera>().enabled = activate;
+    }
+}
