7420b1d470c3253c51c295747c660f25c9c6c8d5
Edvin <edvin.klaebo@gmail.com>
2016-11-19 17:27:12 +0100

    Sounds maybe? XD

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public AudioClip soundClip;
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound()
+    {
+        audio.PlayOneShot(soundClip, 0.7F);
+    }
+
+}
